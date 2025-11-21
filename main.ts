00d0fb9bbaa348ece260dd3b007a2c341b972d2b
mcgilvery-allison <158811057+mcgilvery-allison@users.noreply.github.com>
2025-11-21 14:21:25 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
-
+for (let i = 0; i < screen.width; i++) {
+    for (let j = 0; j < screen.height; j++) {
+        let color = randint(0, 15)
+        scene.backgroundImage().setPixel(i, j, color)
+    }
+}
